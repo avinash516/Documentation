1fa4118c439b1c4859daa68ed97efa348aa2a1f4
Ch.Avinash <37656188+avinash516@users.noreply.github.com>
2020-07-09 15:38:14 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -100,7 +100,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'Djangodoc'
+htmlhelp_basename = 'Djangodocs'
 
 
 # -- Options for LaTeX output ------------------------------------------------
